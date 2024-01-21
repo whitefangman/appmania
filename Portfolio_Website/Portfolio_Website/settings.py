e97b739bd2fa47b978207810610bded05b8f4795
whitefangman <156517289+whitefangman@users.noreply.github.com>
2024-01-22 04:37:53 +0545

    Update settings.py

--- Portfolio_Website/Portfolio_Website/settings.py
+++ Portfolio_Website/Portfolio_Website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8bpy8po#gl9@4sf2+nhn6e1$p5i*z6$a=_n!ey6!76cla38pi('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['creations.pythonanywhere.com']
 
 
 # Application definition
